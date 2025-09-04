d76faad69fa5c1bd27c981435e37f943cae10a36
Norah Efosa <norah.efosa@ag.ch>
2025-09-04 11:36:44 +0000

    store fso birth data as package data

--- data-raw/fso_birth.R
+++ data-raw/fso_birth.R
@@ -4,8 +4,43 @@
 #'
 
 # Load data
-fso_birth <- data.table::fread("data-raw/fso_birth.csv") |> 
-  as_tibble()
+fso_birth_raw <- data.table::fread("data-raw/fso_birth.csv") |>
+  tibble::as_tibble()
+
+fso_municipality <- readxl::read_excel(
+  "data-raw/fso_municipalities_01012023.xlsx"
+) |>
+  tibble::as_tibble() |>
+  dplyr::select(
+    spatial_unit_code = "BFS Gde-nummer", spatial_unit = Gemeindename
+  )
+
+# Prepare births data
+fso_birth <- fso_birth_raw |>
+  # define factor levels for nationality
+  dplyr::mutate(
+    nat = dplyr::case_when(
+      nat_num == 1 ~ "ch",
+      nat_num == 2 ~ "int",
+      .default = "unknown"
+    ),
+    nat = factor(nat, levels = c("ch", "int"))
+  ) |>
+  # add municipality names
+  dplyr::left_join(
+    fso_municipality,
+    by = c("res_mun" = "spatial_unit_code")
+  ) |>
+  # identify municipalities without matches (e.g. die to municipality fusions)
+  dplyr::mutate(
+    spatial_unit = dplyr::case_when(
+      spatial_unit == "Zürich" ~ "Stadt Zürich",
+      is.na(spatial_unit) ~ "unknown",
+      .default = spatial_unit
+    )
+  ) |>
+  dplyr::select(year, spatial_unit, nat, age, n_birth = bir) |>
+  dplyr::arrange(year, spatial_unit, nat, age)
 
 # Add data frames to package
 usethis::use_data(fso_birth, overwrite = TRUE)
